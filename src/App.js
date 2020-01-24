f8569527dd7174b5a80e5499f9c7fc8da650376e
ivers523 <ivers523@umn.edu>
2020-01-23 22:26:56 -0600

    paddle function mapped to keys

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./styles.css"
 import Paddle from "./components/Paddle";
 
 export default function App() {
+  // not the "actual" Y value, rather, these refer to the change in position along the Y axis
+  const [p1PaddleY, setP1PaddleY] = useState(0);
+  const [p2PaddleY, setP2PaddleY] = useState(0);
+
+
+  function handleKey(e) {
+    const char = e.key.toLowerCase();
+    if (char === "w" || char === "s") {
+      setP1PaddleY(p1PaddleY + (char === "w" ? 10 : -10));
+    }
+    if (char === "o" || char === "k"){
+      setP2PaddleY(p2PaddleY + (char === "o" ? 10 : -10));
+
+    }
+  }
+  useEffect(() => {
+    window.addEventListener("keydown", handleKey);
+    return () => window.removeEventListener("keydown", handleKey);
+
+  }, [p1PaddleY, p2PaddleY])
   return (
     <div className="container">
-      <Paddle />
-      <Paddle isPlayerTwo />
+      <Paddle paddleY={p1PaddleY} />
+      <Paddle isPlayerTwo paddleY={p2PaddleY} />
     </div>
   );
 }
--- src/components/Paddle.js
+++ src/components/Paddle.js
@@ -1,6 +1,8 @@
 import React from "react";
 import "./Paddle.css";
 
-export default function Paddle({isPlayerTwo}) {
-  return <div className={isPlayerTwo ? "paddle player2" : "paddle"} />;
+export default function Paddle({ isPlayerTwo, paddleY }) {
+  return <div className={isPlayerTwo ? "paddle player2" : "paddle"}
+    style={{ transform: `translateY(calc(-50% + ${paddleY}px)` }} />;
+
 }
